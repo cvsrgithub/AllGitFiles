f5cb3628586258b03735a3f33c9a31b13ecc8402
cvsrgithub <cvsr07@gmail.com>
2019-09-08 18:06:22 +0530

    adding all

---
+++ code.java
@@ -0,0 +1,5 @@
+System.debug('---Before '+Limits.getDMLRows());
+Delete [Select Id From Account where Id='0017F00001xwPVI'];
+System.debug('---After '++Limits.getDMLRows());
+
+https://github.com/cvsrgithub 
