b92ad2b0f6a96126a0d359c0387aa687ab0b1fb0
cvsrgithub <54940506+cvsrgithub@users.noreply.github.com>
2019-09-08 21:14:50 +0530

    deleted Code.java file

--- code.java
+++
@@ -1,5 +0,0 @@
-System.debug('---Before '+Limits.getDMLRows());
-Delete [Select Id From Account where Id='0017F00001xwPVI'];
-System.debug('---After '++Limits.getDMLRows());
-
-https://github.com/cvsrgithub 
