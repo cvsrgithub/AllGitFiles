dcd7aa6ff85299b8fcbfc67bfbf43978e6740591
cvsrgithub <cvsr07@gmail.com>
2019-09-08 16:30:18 +0530

    files are added

---
+++ GIT_Errors.java
@@ -0,0 +1,22 @@
+Links helped me while learning GIT Hub:
+
+1. if we get 403 error while trying to push into new git hub account then follow this:
+
+Go to: Control Panel -> User Accounts -> Manage your credentials -> Windows Credentials
+
+Under Generic Credentials there are some credentials related to Github, Click on them and click "Remove".
+
+and when you try to push something, you need to login again.
+
+cvsrgithub
+Venkat@1988
+
+2.If you get the below:
+fatal: 'origin' does not appear to be a git repository
+fatal: Could not read from remote repository.
+
+Please make sure you have the correct access rights
+and the repository exists.
+
+Solution: https://stackoverflow.com/questions/32238616/git-push-fatal-origin-does-not-appear-to-be-a-git-repository-fatal-could-nhttps://stackoverflow.com/questions/32238616/git-push-fatal-origin-does-not-appear-to-be-a-git-repository-fatal-could-n
+
